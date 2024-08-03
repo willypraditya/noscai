0e0c45d350e34373a3c11cf646b8aa04eea62576
Willy Praditya <w.praditya@yahoo.com>
2024-08-04 04:16:24 +0700

    init: folder structure, vite-tsconfig-paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
-import './index.css';
+import '@/styles/index.css';
+
+import router from './router/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/pages/Home/Home.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from './Home';
+
+describe('Home', () => {
+  it('should render the title', () => {
+    render(<Home />);
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+});
